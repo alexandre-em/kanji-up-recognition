fd5de64ef3a3930378ee6c446fbbb7f3430855d6
Alexandre <em_alexandre@yahoo.co.jp>
2024-05-17 22:17:28 +0200

    Fix Gradle build bug
    The bug was related to the dependency com.facebook.react:react-native and the version 0.71.0-rc.0 was not found
    
    - Replace ios project language: Objectif C -> Swift

---
+++ ios/KanjiUpRecognition.swift
@@ -0,0 +1,8 @@
+@objc(KanjiUpRecognition)
+class KanjiUpRecognition: NSObject {
+
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
